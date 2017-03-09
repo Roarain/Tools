e005cbc9b35b9864a328c159613de0db29c6f7e0
Roarain <welovewxy@126.com>
2017-03-09 08:52:57 +0800

    Update time1970.py

--- time1970.py
+++ time1970.py
@@ -2,9 +2,9 @@
 import math
 import random
 import time
-def time1970():
+def time1970_10():
     tt = time.time()
     print tt
-def time1970_14():
+def time1970_13():
     tt = str(long(1000 * time.time()))
     print tt
